ba32e7e5a2b80db4b1015b7b7049db5c0d899a96
a-idk <idoko_research@outlookcom>
2023-12-08 06:16:32 -0800

    taskB branch

---
+++ user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Title: Class <User>
+Description: Write a class User that inherits from BaseModel
+Authors: Elizabeth Akindele & Idoko Attah
+"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ User class definition """
+    
+    first_name = ""
+    last_name = ""
+    email = ""
+    password = ""
