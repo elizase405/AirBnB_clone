f73ed41d46cbd85758b16005d297afa88cdc1800
Elizabeth <elizase405@gmail.com>
2023-12-11 08:48:46 -0800

    Used .format instead of f-string

--- models/base_model.py
+++ models/base_model.py
@@ -34,7 +34,7 @@ class BaseModel:
 
     def __str__(self):
         """returns string format"""
-        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         """Updates 'updated_at' with current datetime
